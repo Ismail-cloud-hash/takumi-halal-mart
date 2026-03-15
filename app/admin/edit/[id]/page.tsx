bdd53935bd9865321da55970471aab00abbc09b7
Ismail-cloud-hash <ismailyousufr15@gmail.com>
2026-03-15 15:50:36 +0530

    Added edit product page

---
+++ app/admin/edit/[id]/page.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { supabase } from "../../../../supabase";
+import { useParams } from "next/navigation";
+import Link from "next/link";
+
+export default function EditProduct(){
+
+  const params = useParams();
+  const id = params.id;
+
+  const [name,setName] = useState("");
+  const [price,setPrice] = useState("");
+  const [category,setCategory] = useState("");
+  const [description,setDescription] = useState("");
+
+  useEffect(()=>{
+
+    async function fetchProduct(){
+
+      const {data} = await supabase
+        .from("products")
+        .select("*")
+        .eq("id",id)
+        .single();
+
+      if(data){
+        setName(data.name);
+        setPrice(data.price);
+        setCategory(data.category);
+        setDescription(data.description);
+      }
+
+    }
+
+    fetchProduct();
+
+  },[id]);
+
+
+  async function updateProduct(){
+
+    await supabase
+      .from("products")
+      .update({
+        name,
+        price,
+        category,
+        description
+      })
+      .eq("id",id);
+
+    alert("Product Updated");
+
+  }
+
+
+  return(
+
+    <main className="p-10 max-w-md">
+
+      <h1 className="text-3xl font-bold mb-6">
+        Edit Product
+      </h1>
+
+      <input
+        className="w-full p-3 bg-gray-800 rounded mb-3"
+        value={name}
+        onChange={(e)=>setName(e.target.value)}
+      />
+
+      <input
+        className="w-full p-3 bg-gray-800 rounded mb-3"
+        value={price}
+        onChange={(e)=>setPrice(e.target.value)}
+      />
+
+      <input
+        className="w-full p-3 bg-gray-800 rounded mb-3"
+        value={category}
+        onChange={(e)=>setCategory(e.target.value)}
+      />
+
+      <input
+        className="w-full p-3 bg-gray-800 rounded mb-3"
+        value={description}
+        onChange={(e)=>setDescription(e.target.value)}
+      />
+
+      <button
+        onClick={updateProduct}
+        className="bg-green-600 px-6 py-3 rounded"
+      >
+        Update Product
+      </button>
+
+      <div className="mt-6">
+        <Link href="/admin" className="text-blue-400">
+          Back to Admin
+        </Link>
+      </div>
+
+    </main>
+
+  )
+
+}
